1c131a25ab694b006d7a0bac3450b8cee90e726d
Skrippe3 <97904563+Skrippe3@users.noreply.github.com>
2025-02-17 14:31:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsAlgotims
+{
+    internal class Program
+    {
+        static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        public static int[] BubbleSort(int[] array)
+        {
+            int len = array.Length;
+
+            for (int i = 1; i < len; i++) 
+            {
+                for (int j = 0; j < len - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 21, 3, 6, 5, 2, 9, 12, 4, 8 };
+
+            int[] NewArr = BubbleSort(arr);
+            Console.WriteLine(string.Join(", ", NewArr));
+        }
+    }
+}
