19313b5a31ac1b588ca24f8f38501e4fcd90a3dc
Skrippe3 <97904563+Skrippe3@users.noreply.github.com>
2025-03-16 14:12:30 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace LabsAlgotims
+{
+    class Class1
+    {
+        //Алгоритм машины тьюринга
+        public static int LABS2(int y, int x)
+        {
+            if (y <= x)
+            {
+                return x ^ 2 + y ^ 2;
+            }
+            if (y > x)
+            {
+                return y / x;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
